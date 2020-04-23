373f472e86ab2fe24bbe604c038b42d9869aba7b
Camilo Davila <camilo7davila@gmail.com>
2020-04-23 11:08:02 -0500

    login completo

--- components/administrador/store.js
+++ components/administrador/store.js
@@ -1,4 +1,5 @@
 const Model = require('./model')
+const ModelParent = require('../parents/model')
 
 function addAdmin(admin){
     const myUser = new Model(admin);
@@ -7,7 +8,29 @@ function addAdmin(admin){
 
 function emailExist(email){
     return new Promise((resolve, reject) => {
-        Model.findOne({email: email}).exec( (err,data) => {
+        Model.findOne({email: email}).exec( async (err,data) => {
+            if(err){
+                reject('Ocurrio un error')
+            }
+
+            if(!data){
+                try{
+                    const final = await emailParentExist(email)
+                    resolve(final)
+                }catch{
+                    reject('no se encontro nada')
+                }
+            }
+
+            resolve(data)
+        })
+    })
+}
+
+function emailParentExist(email){
+    return new Promise((resolve, reject) => {
+        ModelParent.findOne({email: email}).exec( (err,data) => {
+            console.log('esto es data => ' +data);
             if(err){
                 reject('Ocurrio un error')
             }
